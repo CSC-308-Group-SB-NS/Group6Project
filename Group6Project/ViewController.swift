0d35c67cd87b8a8b81895ae736b756504abc1711
Sam <CSC308SamB>
2026-04-16 11:05:52 -0400

    Initial Commit

---
+++ Group6Project/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Group6Project
+//
+//  Created by Boston, Samuel T. on 4/16/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // Test
+    let cars = ["2010 BMW", "2005 BMW", "2012 BMW", "2025 BMW"]
+    let prices = ["$20,000-$40,000", "$10,000-$20,000", "$30,000-$50,000", "$50,000-$70,000"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension ViewController: UITableViewDataSource
+{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return cars.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "vehicleCell", for: indexPath)
+        
+        cell.textLabel?.text = cars[indexPath.section]
+        cell.detailTextLabel?.text = prices[indexPath.section]
+        cell.imageView?.image = UIImage(systemName: "car.fill")
+        
+        return cell
+    }
+    
+}
