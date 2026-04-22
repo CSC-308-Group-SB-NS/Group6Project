f85a5840b58fbab1b9030673cabe384eaa4612da
Sam <CSC308SamB>
2026-04-21 22:56:34 -0400

    Final Tweaks

--- Group6Project/BuyViewController.swift
+++ Group6Project/BuyViewController.swift
@@ -28,7 +28,7 @@ class BuyViewController: UIViewController {
         
         nameLabel.text = carName
         costLabel.text = carPrice
-        descriptionLabel.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ligula mi, bibendum vel libero eget, pellentesque fermentum lorem. Nam vitae ipsum sed nunc pulvinar elementum sed quis augue. Pellentesque finibus commodo mauris, eu tristique nunc mattis eu. Suspendisse finibus vitae lacus non egestas. Duis iaculis leo eu magna venenatis, a imperdiet dui lobortis. Nulla bibendum lacus in diam malesuada, ut feugiat nibh sollicitudin. Phasellus ultricies lorem sed quam luctus dictum. Morbi maximus viverra dui, id bibendum ligula finibus a. Pellentesque a nulla id leo luctus maximus id quis justo. Morbi maximus euismod enim, at dictum nisl congue a. Vivamus egestas quam sit amet lorem facilisis, ut cursus mi convallis. Aliquam et nisl a metus ultricies convallis sit amet eu lorem. Nam aliquet ex eget ultricies venenatis."
+        descriptionLabel.text = "This is a general description of the car, including its model and some information about it!"
         
         imageView.image = carImage
     }
